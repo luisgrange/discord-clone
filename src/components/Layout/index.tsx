b3f804699dfc62bb4ab628ffac02abf69fabf420
luisgrange <luisfernando237grange@outlook.com>
2022-06-14 14:55:53 -0300

    adicionando componentes

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Grid } from './styles';
+
+export function Layout(){
+    return(
+        <Grid/>
+    );
+}
---
+++ src/components/Layout/styles.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+//SL - server list
+//SN - Server name
+//CI - channel info
+//CL - channel list
+//CD - channel data
+//UL - users list
+//UI - user info
+
+export const Grid = styled.div`
+    display: grid;
+
+    grid-template-columns: 71px 240px auto 250px;
+    grid-template-rows: 46px auto 52px;
+
+    grid-template-areas: 
+    'SL SN CI CI'
+    'SL CL CD UL'
+    'SL UI CD UL';
+
+    height: 100vh;
+`
