dac231f0d0692a2efeb20828273c371056a93ea4
luisgrange <luisfernando237grange@outlook.com>
2022-06-17 09:34:15 -0300

    Estilizando barra de chat

--- src/components/ChannelData/ChatBar/styles.tsx
+++ src/components/ChannelData/ChatBar/styles.tsx
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 import { At } from 'styled-icons/evaicons-solid';
 
 export const Container = styled.div`
-    background-color: transparent;
+    background-color: var(--primary);
     width: 100%;
-    flex: 1;
+    
 
     position: absolute;
     bottom: 0;
 
-    padding: 0 16px 23px;
+    padding: 13px 16px 13px;
 
     > form{
         width: 100%;
@@ -26,14 +26,17 @@ export const Container = styled.div`
         background-color: #40444B;
         border-radius: 7px;
 
-        color: #6f6f6a;
+        color: #b2b2b2;
 
         
+    }
+    > form input::placeholder{
+        color: #b2b2b2;
     }
 `;
 
 export const AtIcon = styled(At)`
-    color: #6f6f6a;
+    color: #b2b2b2;
 
     position: absolute;
     left: 16px;
